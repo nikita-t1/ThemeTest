b3c0106352b305d86e1bd8d4c1729575d2d0dbde
nikita-t1 <nikita.t2@gmx.de>
2020-06-06 23:41:08 +0200

    Switch between Themes

--- app/src/main/java/com/example/themetest/MainActivity.kt
+++ app/src/main/java/com/example/themetest/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.themetest
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
@@ -10,18 +12,39 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    val prefs by lazy {
+        this.getSharedPreferences("PREFS_NAME", Context.MODE_PRIVATE)
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val i = mutableListOf(R.style.AppTheme_NoActionBar, R.style.AppTheme2)
-        val o = i.shuffled()[0]
-        setTheme(o)
+        val theme = if (read("key", true)){
+            R.style.AppTheme_NoActionBar
+        } else {
+            R.style.AppTheme2
+        }
+        setTheme(theme)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
-            finish()
-            startActivity(Intent(this@MainActivity, this@MainActivity.javaClass))
+            val reverse = !read("key", true)
+            write("key", reverse)
+            recreate()
+//            finish()
+//            startActivity(Intent(this@MainActivity, this@MainActivity.javaClass))
         }
+    }
+
+    fun read(key: String, value: Boolean): Boolean {
+        return prefs.getBoolean(key, value)
+    }
+
+    fun write(key: String, value: Boolean) {
+        val prefsEditor: SharedPreferences.Editor = prefs.edit()
+        prefsEditor.putBoolean(key, value)
+        prefsEditor.apply()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
