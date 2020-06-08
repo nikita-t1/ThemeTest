6b82861258f83b16bbdc0fe24f8095d2309a0c97
nikita-t1 <nikita.t2@gmx.de>
2020-06-08 23:21:53 +0200

    Added Animations

--- app/src/main/java/com/example/themetest/MainActivity.kt
+++ app/src/main/java/com/example/themetest/MainActivity.kt
@@ -4,10 +4,16 @@ import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
+import android.view.*
+import android.view.ViewTreeObserver.OnGlobalLayoutListener
+import android.view.animation.AccelerateInterpolator
 import androidx.appcompat.app.AppCompatActivity
+import androidx.coordinatorlayout.widget.CoordinatorLayout
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.math.max
 
 
 class MainActivity : AppCompatActivity() {
@@ -15,28 +21,72 @@ class MainActivity : AppCompatActivity() {
     val prefs by lazy {
         this.getSharedPreferences("PREFS_NAME", Context.MODE_PRIVATE)
     }
-
+    lateinit var rootLayout: CoordinatorLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val theme = if (read("key", true)){
+        val theme = if (read("key", true)) {
             R.style.AppTheme_NoActionBar
         } else {
             R.style.AppTheme2
         }
         setTheme(theme)
         setContentView(R.layout.activity_main)
+
+//        rootLayout = findViewById(R.id.main)
+//        rootLayout.visibility = View.INVISIBLE
+//        val viewTreeObserver: ViewTreeObserver = rootLayout.getViewTreeObserver()
+//        if (viewTreeObserver.isAlive) {
+//            viewTreeObserver.addOnGlobalLayoutListener(object : OnGlobalLayoutListener {
+//                override fun onGlobalLayout() {
+//                    revealActivity(
+//                        resources.displayMetrics.widthPixels / 2,
+//                        resources.displayMetrics.heightPixels / 2
+//                    )
+//                    rootLayout.viewTreeObserver.removeOnGlobalLayoutListener(this)
+//                }
+//            })
+//        }
+
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            val reverse = !read("key", true)
-            write("key", reverse)
-            recreate()
-//            finish()
-//            startActivity(Intent(this@MainActivity, this@MainActivity.javaClass))
+        fab.setOnClickListener {
+            createIntend(R.anim.fragment_open_enter, R.anim.fragment_open_exit)
+        }
+        arrow.setOnClickListener {
+                createIntend(R.anim.fragment_fade_enter, R.anim.fragment_fade_exit)
         }
     }
 
+    private fun createIntend(fragmentOpenEnter: Int, fragmentOpenExit: Int) {
+        val reverse = !read("key", true)
+        write("key", reverse)
+
+        val intend = Intent(this@MainActivity, this@MainActivity.javaClass)
+        startActivity(intend)
+        overridePendingTransition(fragmentOpenEnter, fragmentOpenExit)
+        finish()
+    }
+
+
+    private fun revealActivity(x: Int, y: Int) {
+            val finalRadius = (max(
+                rootLayout.width,
+                rootLayout.height
+            ) * 1.1).toFloat()
+
+            // create the animator for this view (the start radius is zero)
+            val circularReveal =
+                ViewAnimationUtils.createCircularReveal(rootLayout, x, y, 0f, finalRadius)
+            circularReveal.duration = 400
+            circularReveal.interpolator = AccelerateInterpolator()
+
+            // make the view visible and start the animation
+        rootLayout.visibility = View.VISIBLE
+            circularReveal.start()
+    }
+
+    
     fun read(key: String, value: Boolean): Boolean {
         return prefs.getBoolean(key, value)
     }
