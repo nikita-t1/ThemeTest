ca7ddd3c33ab12332839644a6d203ac7d384c57c
nikita-t1 <nikita.t2@gmx.de>
2020-06-10 23:21:33 +0200

    Fragment Theme

--- app/src/main/java/com/example/themetest/SecondFragment.kt
+++ app/src/main/java/com/example/themetest/SecondFragment.kt
@@ -1,12 +1,15 @@
 package com.example.themetest
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.appcompat.view.ContextThemeWrapper
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -17,8 +20,16 @@ class SecondFragment : Fragment() {
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
+
+        // create ContextThemeWrapper from the original Activity Context with the custom theme
+        val contextThemeWrapper: Context = ContextThemeWrapper(requireActivity(), R.style.AppTheme3)
+
+        // clone the inflater using the ContextThemeWrapper
+        val localInflater = inflater.cloneInContext(contextThemeWrapper)
+
+        // inflate the layout using the cloned inflater, not default inflater
+        return localInflater.inflate(R.layout.fragment_second, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
